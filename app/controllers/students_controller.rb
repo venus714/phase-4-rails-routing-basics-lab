7f462bdbc4d7921e7ac04c8354394fa997dc1676
Student <venus.kabera@student.moringaschool.com>
2023-03-13 12:21:47 +0300

    done

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,16 @@
+class StudentsController < ApplicationController
+    def index
+    students = Student.all
+    render json: students
+end
+    
+    def grades
+        students = Student.order(grade: :desc)
+        render json: students
+      end
+    
+     
+    
+    end
+    
+
